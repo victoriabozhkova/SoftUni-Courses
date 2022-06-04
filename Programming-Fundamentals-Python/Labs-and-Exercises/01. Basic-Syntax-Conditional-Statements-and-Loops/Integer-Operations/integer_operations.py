fe67747d91f36cb021b386698f5c52fe85209cac
victoriabozhkova <vickyyy.vd@gmail.com>
2022-06-04 07:43:53 +0300

    Added exercises

---
+++ Programming-Fundamentals-Python/Labs-and-Exercises/01. Basic-Syntax-Conditional-Statements-and-Loops/Integer-Operations/integer_operations.py
@@ -0,0 +1,8 @@
+first_int = int(input())
+second_int = int(input())
+third_int = int(input())
+fourth_int = int(input())
+
+result = (first_int + second_int) // third_int * fourth_int
+
+print(result)
